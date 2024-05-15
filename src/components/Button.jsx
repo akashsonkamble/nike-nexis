53aed919c2da940f3168356a5471dfe6d2cf2ad0
akash <akashsonkamble199@gmail.com>
2024-05-15 14:12:23 +0530

    feat: Implement SpecialOffer.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,28 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${borderColor} ${textColor}`
+        : "bg-coral-red text-white border-coral-red"
+    } rounded-full w-full"}`}
+    >
       {label}
-      {iconURL && (<img
-        src={iconURL}
-        alt="arrow right icon"
-        width={20}
-        height={20}
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          width={20}
+          height={20}
+          className="ml-2 rounded-full w-5 h-5"
+        />
       )}
     </button>
   );
